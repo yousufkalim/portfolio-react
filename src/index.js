a83d610660712ed0f2d28bc19c3754db05933e12
Yousuf Kalim <yousufkalim@outlook.com>
2020-11-23 13:07:57 +0500

    Setted up Index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//Init
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+//Render
+ReactDOM.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById("root")
+);
