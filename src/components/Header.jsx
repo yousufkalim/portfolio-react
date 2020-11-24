76c639c2c884fad060a52e1b364c4fcd235bfe1e
Yousuf Kalim <yousufkalim@outlook.com>
2020-11-24 11:12:51 +0500

    Added Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+//Style
+import "../Style/Header.css";
+
+function Header() {
+	return (
+		<header>
+			<div className="header-title">
+				<span>A FREELANCER</span>
+			</div>
+			<div className="header-developer">
+				<span>Web Developer</span>
+			</div>
+			<div className="header-qoute">
+				<span>IF IT DOESN’T CHALLENGE YOU, IT DOESN’T CHANGE YOU</span>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,13 @@ import React from "react";
 
 //Components
 import Nav from "../components/Nav";
+import Header from "../components/Header";
 
 function Home() {
 	return (
 		<React.Fragment>
 			<Nav />
+			<Header />
 		</React.Fragment>
 	);
 }
