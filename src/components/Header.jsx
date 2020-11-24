02f0e0fe48c66211db4f7e6a53c2d6e40d4f6892
Yousuf Kalim <yousufkalim@outlook.com>
2020-11-24 13:42:06 +0500

    Header background fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,18 @@ import "../Style/Header.css";
 function Header() {
 	return (
 		<header>
-			<div className="header-title">
-				<span>A FREELANCER</span>
-			</div>
-			<div className="header-developer">
-				<span>Web Developer</span>
-			</div>
-			<div className="header-qoute">
-				<span>IF IT DOESN’T CHALLENGE YOU, IT DOESN’T CHANGE YOU</span>
+			<div className="header-container">
+				<div className="header-title">
+					<span>A FREELANCER</span>
+				</div>
+				<div className="header-developer">
+					<span>Web Developer</span>
+				</div>
+				<div className="header-qoute">
+					<span>
+						IF IT DOESN’T CHALLENGE YOU, IT DOESN’T CHANGE YOU
+					</span>
+				</div>
 			</div>
 		</header>
 	);
