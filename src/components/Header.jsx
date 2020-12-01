eabea630c7ca67aa98e96e65d42d2b02a9b275e5
Yousuf Kalim <yousufkalim@outlook.com>
2020-12-01 16:51:48 +0500

    Added transition in Headers content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
 //Init
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //Style
 import "../Style/Header.css";
 
 //Component
 function Header({ heading, title, qoute }) {
+	//Initializing State to Animate Header
+	let [visible, setVisible] = useState(false);
+
+	useEffect(() => {
+		setVisible(true);
+	}, []);
+
 	return (
 		<header id="header">
-			<div className="header-container">
+			<div className={`header-container ${visible && "header-true"}`}>
 				<div className="header-title">
 					<span>{heading}</span>
 				</div>
 				<div className="header-developer">
 					<span>{title}</span>
 				</div>
-				<div className="header-qoute">
+				<div className="header-quote">
 					<span>{qoute}</span>
 				</div>
 			</div>
--- src/components/HeaderGlobal.jsx
+++ src/components/HeaderGlobal.jsx
@@ -1,11 +1,18 @@
 //Init
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //Style
 import "../Style/HeaderGlobal.css";
 
 //Component
 function HeaderGlobal({ inPortfolio, title, quote }) {
+	//Initializing State to Animate Header Content
+	let [visible, setVisible] = useState(false);
+
+	useEffect(() => {
+		setVisible(true);
+	}, []);
+
 	return (
 		<header id="header-global">
 			<div
@@ -13,11 +20,17 @@ function HeaderGlobal({ inPortfolio, title, quote }) {
 					inPortfolio && "inPortfolio"
 				}`}
 			>
-				<div className="header-global-title">
-					<span>{title}</span>
-				</div>
-				<div className="header-global-qoute">
-					<span>{quote}</span>
+				<div
+					className={`header-global-transition ${
+						visible && "header-global-true"
+					}`}
+				>
+					<div className="header-global-title">
+						<span>{title}</span>
+					</div>
+					<div className="header-global-qoute">
+						<span>{quote}</span>
+					</div>
 				</div>
 			</div>
 		</header>
