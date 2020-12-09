ce76b645645933dc2e87ca59edb578648f4167bb
Yousuf Kalim <yousufkalim@outlook.com>
2020-12-09 08:43:46 +0500

    Saperated Portal from this website

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,7 +5,6 @@ import { Switch, Route } from "react-router-dom";
 //Importing Routes
 import PortfolioRoutes from "./PortfolioRoutes";
 import BlogRoutes from "./BlogRoutes";
-import PortalRoutes from "./PortalRoutes";
 
 //Pages
 import Home from "../pages/Home";
@@ -13,7 +12,6 @@ import Recent from "../pages/Recent";
 import Articles from "../pages/Articles";
 import ContactMe from "../pages/ContactMe";
 import ErrorPage from "../pages/ErrorPage";
-import Portal from "../portal/Portal";
 
 //Component
 function PageRoutes() {
@@ -25,8 +23,6 @@ function PageRoutes() {
 			<Route exact path="/blog" component={Articles} />
 			<Route exact path="/blog/*" component={BlogRoutes} />
 			<Route exact path="/contact" component={ContactMe} />
-			<Route exact path="/portal" component={Portal} />
-			<Route exact path="/portal/*" component={PortalRoutes} />
 			<Route exact path="/*" component={ErrorPage} />
 		</Switch>
 	);
